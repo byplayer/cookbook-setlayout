dd38e7a4a6c62f7d3a318a76821f1bfaae8d26b6
byplayer <byplayer100@gmail.com>
2015-12-17 15:35:57 +0800

    add install setlayout recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,38 @@
 # Cookbook Name:: cookbook-setlayout
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, byplayer
 #
 # All rights reserved - Do Not Redistribute
 #
+packages = case node['platform_family']
+           when 'rhel'
+             %w(libevent-devel ncurses-devel gcc make)
+           else
+             %w(libevent-dev libncurses5-dev gcc make)
+           end
+
+packages.each do |name|
+  package name
+end
+
+source_url = node['setlayout']['source_url'] % { :version => node['setlayout']['version'] }
+file_name = source_url.split('/').last
+remote_file "#{Chef::Config['file_cache_path']}/#{file_name}" do
+  source source_url
+  checksum node['setlayout']['checksum']
+  notifies :run, 'bash[install_setlayout]', :immediately
+end
+
+bash 'install_setlayout' do
+  user 'root'
+  cwd Chef::Config['file_cache_path']
+  code <<-EOH
+      tar -zxf #{file_name}
+      cd #{file_name.gsub(/\.tar\.gz/, '')}
+      ./configure #{node['tmux']['configure_options'].join(' ')}
+      make
+      make install
+    EOH
+  action :nothing
+end
