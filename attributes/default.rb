880d2e36c67449952427adeac5fc21956491de41
byplayer <byplayer100@gmail.com>
2015-12-17 17:56:30 +0800

    fix url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['setlayout']['source_url'] = 'https://github.com/byplayer/setlayout/archive/v%s.tar.gz'
+default['setlayout']['source_url'] = 'https://github.com/byplayer/setlayout/archive/v%{version}.tar.gz'
 default['setlayout']['version'] = '1.0'
 default['setlayout']['checksum'] = 'c82ce81bf979b83c2be64da7267766d2946cba2d8e1b7420c9dd3660228322c5'
 default['setlayout']['configure_options'] = ['--prefix=/opt/setlayout']
