27ee5212108765b528af6eb6437efdc1186d9807
byplayer <byplayer100@gmail.com>
2015-12-17 15:55:53 +0800

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
-name             'setlayout'
-maintainer       'byplayer'
+name 'setlayout'
+maintainer 'byplayer'
 maintainer_email 'byplayer100@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures cookbook-setlayout'
+license 'Apache v2.0'
+description 'Installs/Configures setlayout'
+supports 'ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version '0.1.0'
